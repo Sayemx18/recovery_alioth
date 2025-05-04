ac6c1e4149483946f8f6f7e76b546d4af36dcff0
Darth9 <reyprimerjefe@gmail.com>
2025-05-04 21:46:39 +0100

    Add the fsck.erofs binary

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -64,6 +64,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     	export FOX_DELETE_AROMAFM=1
 	export TARGET_DEVICE_ALT="aliothin,alioth"
 
+	export FOX_USE_FSCK_EROFS_BINARY=1
+
 	# instruct magiskboot v24+ to always patch the vbmeta header when patching the recovery/boot image; do *not* remove!
         export FOX_PATCH_VBMETA_FLAG="1"
 
