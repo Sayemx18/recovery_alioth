0ac346ba10e770ebc7ff6f7705ebb73e489e1cd4
Darth9 <reyprimerjefe@gmail.com>
2025-10-20 15:13:34 +0100

    Update the magisk addon to v30.4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,7 +48,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		exit 1
 	fi
 
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v28.1.zip
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.4.zip
         export FOX_VANILLA_BUILD=1
     	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_VIRTUAL_AB_DEVICE=1
@@ -71,7 +71,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 	# vendor_boot-as-recovery
 	if [ "$FOX_VENDOR_BOOT_RECOVERY" = "1" ]; then
-	   export FOX_VARIANT="vBaR"
+		export FOX_VARIANT="vBaR"
 	fi
 else
 	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
