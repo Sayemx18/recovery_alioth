d0609960336731706368c93f0e82dc163e460110
Darth9 <reyprimerjefe@gmail.com>
2026-01-01 21:29:23 +0000

    Enable the FRP removal addon

--- fox_mikona.mk
+++ fox_mikona.mk
@@ -1,6 +1,6 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2023-2025 The OrangeFox Recovery Project
+# 	Copyright (C) 2023-2026 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -62,4 +62,7 @@ OF_ENABLE_ALL_PARTITION_TOOLS := 1
 
 # add dmctl
 OF_USE_DMCTL := 1
+
+# frp
+OF_ENABLE_FRP_ADDON := 1
 #
